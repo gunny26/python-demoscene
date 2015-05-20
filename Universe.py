2c14f207dc91f4699a3ce8bca2affad1001ed606
Arthur Messner <arthur.messner@gmail.com>
2015-05-20 22:03:07 +0200

    automatic commit

--- Universe.py
+++ Universe.py
@@ -13,36 +13,37 @@ class Thing(object):
     the thing at starting point moves randomly
     """
 
-    def __init__(self, surface, universe, pos3d, color):
+    def __init__(self, surface, universe, pos2d, color):
         self.surface = surface
         self.universe = universe
-        self.pos3d = pos3d
+        self.pos2d = pos2d
         self.mass = random.random()
-        self.color = pygame.Color(int(128 + 127 * self.mass), 255, 255, 255)
+        print self.mass
+        self.acceleration = random.random()
+        self.direction = Vec2d(random.random(), random.random()) * self.mass
+        self.color = pygame.Color(int(128 + 127 * self.mass), 0, 0, 255)
 
     def draw(self, viewer_distance, fov):
-        t_pos3d = self.pos3d.project(self.surface.get_width(), self.surface.get_height(), viewer_distance, fov)
-        t_pos2d = Vec2d(t_pos3d.x, t_pos3d.y)
-        pygame.draw.line(self.surface, self.color, t_pos2d, t_pos2d, 1)
-        
+        color = pygame.Color(int(255 * self.mass), int(255 * self.acceleration), 0, 255)
+        pygame.draw.line(self.surface, color, self.pos2d, self.pos2d, 2)
+
     def update(self):
         #print self.pos3d
-        new_direction = None
+        new_direction = self.direction
         for other_thing in self.universe.things:
             if other_thing == self:
                 continue
-            if other_thing.pos3d == self.pos3d:
-                self.mass += other_thing.mass
-                print "removing %s" % other_thing
-                self.universe.remove(other_thing)
-                continue
+            #if int(other_thing.pos2d.x) == int(self.pos2d.x) and int(other_thing.pos2d.y) == int(self.pos2d.y):
+            #    self.mass += other_thing.mass
+            #    print "removing %s" % other_thing
+            #    self.universe.things.remove(other_thing)
+            #    continue
             direction = other_thing.get_direction_to(self)
-            distance = direction.length
-            if new_direction is None:
-                new_direction = direction.normalized()
-            else:
-                new_direction = (new_direction + (direction * other_thing.mass)).normalized()
-        self.pos3d += new_direction
+            new_direction += direction * other_thing.mass
+        #pygame.draw.line(self.surface, pygame.Color(0, 0, 255, 0), self.pos2d, self.pos2d + new_direction.normalized() * 10, 1)
+        #self.acceleration *= (1 - self.mass)
+        self.check_boundaries()
+        self.pos2d += (new_direction * self.acceleration).normalized()
 
     def get_nearest_thing(self):
         min_dist = None
@@ -55,19 +56,94 @@ class Thing(object):
             if (min_dist is None) or (min_dist > distance):
                 nearest_thing = other_thing
                 min_dist = distance
-        return(nearest_thing)
+        return nearest_thing
+
+    def check_boundaries(self):
+        """
+        check if position is outside of visible surface,
+        and change direction accordingly
+        """
+        newpos = self.pos2d + self.direction
+        if (newpos.x < 0) or (newpos.x > self.surface.get_width()):
+            self.direction.x *= -1
+        if (newpos.y < 0) or (newpos.y > self.surface.get_height()):
+            self.direction.y *= -1
 
     def get_direction_to(self, other):
-        return(other.pos3d - self.pos3d)
+        return self.pos2d - other.pos2d
 
     def get_distance_to(self, other):
-        return(self.get_direction_to().length)
+        return self.get_direction_to(other).length
+
+class Neutron(Thing):
+    """
+    the thing at starting point moves randomly
+    """
+
+    def __init__(self, surface, universe, pos2d, color):
+        Thing.__init__(self, surface, universe, pos2d, color)
+        self.kind = "Proton"
+
+    def draw(self, viewer_distance, fov):
+        color = pygame.Color(0, 0, int(255 * self.mass), int(255 * self.acceleration))
+        pygame.draw.line(self.surface, color, self.pos2d, self.pos2d, 2)
+
+
+    def get_direction_to(self, other):
+        return Vec2d(0.0, 0.0)
+
+    def get_distance_to(self, other):
+        return self.get_direction_to(other).length
+
+
+
+class Proton(Thing):
+    """
+    the thing at starting point moves randomly
+    """
+
+    def __init__(self, surface, universe, pos2d, color):
+        Thing.__init__(self, surface, universe, pos2d, color)
+        self.kind = "Proton"
+
+    def draw(self, viewer_distance, fov):
+        color = pygame.Color(0, int(255 * self.mass), int(255 * self.acceleration), 255)
+        pygame.draw.line(self.surface, color, self.pos2d, self.pos2d, 2)
+
+
+    def get_direction_to(self, other):
+        if other.kind != self.kind:
+            return self.pos2d - other.pos2d
+        return other.pos2d - self.pos2d
+
+    def get_distance_to(self, other):
+        return self.get_direction_to(other).length
+
+
+class Electron(Thing):
+    """
+    the thing at starting point moves randomly
+    """
+
+    def __init__(self, surface, universe, pos2d, color):
+        Thing.__init__(self, surface, universe, pos2d, color)
+        self.kind = "Electron"
+
+    def get_direction_to(self, other):
+        if other.kind != self.kind:
+            return self.pos2d - other.pos2d
+        return other.pos2d - self.pos2d
+
+    def get_distance_to(self, other):
+        return self.get_direction_to(other).length
 
 
 class Universe(object):
-    """Starfield with 3D Points"""
+    """Universe with Things"""
 
-    def __init__(self, surface, stars, depth, speed=0.01):
+    kinds = (Proton, Electron, Neutron)
+
+    def __init__(self, surface, stars, speed=0.01):
         """
         surface = pygame.Surface
         stars - amount of strar to create
@@ -76,7 +152,6 @@ class Universe(object):
         """
         self.surface = surface
         self.stars = stars
-        self.depth = depth
         self.speed = speed
         # set initial variables
         self.color = pygame.Color(255, 255, 255, 255)
@@ -86,12 +161,10 @@ class Universe(object):
 
     def generate(self):
         """ generates 3d starfield, z=0 to z=depth """
-        depth_count = self.stars / self.depth
-        for index in range(depth_count ** 3):
-            star_x = random.random() * 4 - 2
-            star_y = random.random() * 4 - 2
-            star_z = random.random() * 4
-            self.things.append(Thing(self.surface, self, Vec3d(star_x, star_y, star_z), self.color))
+        for index in range(self.stars):
+            index = int(random.random() * len(self.kinds))
+            pos2d = Vec2d(random.random() * self.surface.get_width(), random.random() * self.surface.get_height())
+            self.things.append(self.kinds[index](self.surface, self, pos2d, self.color))
 
     def update(self, fov=2, viewer_distance=256):
         """update every frame"""
@@ -103,22 +176,22 @@ class Universe(object):
 def main():
     """test"""
     try:
-        fps = 1000
+        fps = 30
         surface = pygame.display.set_mode((600, 600))
         pygame.init()
         spheres = (
-            Universe(surface, stars=50, depth=10, speed=0.01),
+            Universe(surface, stars=50, speed=0.01),
             )
-        clock = pygame.time.Clock()       
+        clock = pygame.time.Clock()
         # for 3d projection
         fov = 1
         viewer_distance = 256
         pause = False
         while True:
             clock.tick(fps)
-            events = pygame.event.get()  
-            for event in events:  
-                if event.type == pygame.QUIT:  
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
                     sys.exit(0)
             keyinput = pygame.key.get_pressed()
             if keyinput is not None:
